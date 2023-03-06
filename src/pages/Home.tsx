429846e2f2782f8dead1e0a4bbc99d12819233ae
özkan demir <ozkanndemir2009@gmail.com>
2023-03-06 22:08:42 +0300

    components and pages added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SelectForm from '../components/SelectForm'
+import TodoList from '../components/TodoList'
+
+const Home = () => {
+  return (
+    <div>
+        <SelectForm/>
+        <TodoList/>
+    </div>
+  )
+}
+
+export default Home
